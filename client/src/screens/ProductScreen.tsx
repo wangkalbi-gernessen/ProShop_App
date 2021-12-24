1178f7036064ca3d90acd8330b093fb0625274ab
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-12-24 00:03:43 -0800

    Populate fetched data into empty object on ProductScreen

--- client/src/screens/ProductScreen.tsx
+++ client/src/screens/ProductScreen.tsx
@@ -7,36 +7,34 @@ const ProductScreen = ({props}: any) => {
   const [product, setProduct] = useState({ });
   const { id } = useParams();
   const toNum = Number(id);
-  // console.log(toNum);
-  // const rating = Number(product?.rating);
-  // const stock = Number(product?.countInStock);
+  const rating = Number((product as any).rating);
+  const stock = Number((product as any).countInStock);
 
   useEffect(() => {
     const fetchProduct = async() => {
       const { data } = await axios.get(`/api/products/${toNum}`);
-       setProduct(data);
+      // console.log(data);
+      setProduct({...product, ...data});
     }
     fetchProduct();
   },[]);
 
-  console.log(product);
-  
   return (
     <>
       <Link to='/' className='btn btn-dark my-3' >Go Back</Link>
       <div className="row">
         <div className="col-md-6">
-          {/* <img src={product} alt={product} className="img-fluid" /> */}
+          <img src={(product as any).image} alt={(product as any).image} className="img-fluid" />
         </div>
         <div className="col-md-3">
           <ul className="list-group list-group-flush">
-            {/* <li className='list-group-item'>{ product?.name }</li> */}
+            <li className='list-group-item'>{ (product as any).name }</li>
             <li className='list-group-item'>
-              {/* {product?.rating} */}
-              {/* <Rating value={rating } text={`${product?.numReviews} reveiws`} color="red" /> */}
+              {(product as any).rating}
+              <Rating value={rating } text={`${(product as any).numReviews} reveiws`} color="red" />
             </li>
-            {/* <li className='list-group-item'>Price: ${product?.price}</li> */}
-            {/* <li className='list-group-item'>Description: {product?.description}</li> */}
+            <li className='list-group-item'>Price: ${(product as any).price}</li>
+            <li className='list-group-item'>Description: {(product as any).description}</li>
           </ul>
         </div>
         <div className="col-md-3">
@@ -48,7 +46,7 @@ const ProductScreen = ({props}: any) => {
                     Price: 
                   </div>
                   <div className="col">
-                    {/* <p><strong>${product?.price}</strong></p>  */}
+                    <p><strong>${(product as any).price}</strong></p> 
                   </div>
                 </div>
               </li>
@@ -58,13 +56,13 @@ const ProductScreen = ({props}: any) => {
                     Status: 
                   </div>
                   <div className="col">
-                    {/* { stock > 0 ? 'In stock' : 'Out of Stock'} */}
+                    { stock > 0 ? 'In stock' : 'Out of Stock'}
                   </div>
                 </div>
               </li>
               <li className='list-group-item'>
                 <div className="d-grid gap-2 col-12 mx-auto">
-                  {/* <button className="btn btn-dark" type="button" disabled={stock === 0}>Add To Cart</button> */}
+                  <button className="btn btn-dark" type="button" disabled={stock === 0}>Add To Cart</button>
                 </div>
               </li>
             </ul>
