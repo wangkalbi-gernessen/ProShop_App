6d1c9bb8e3979c1506ebc7c352a4a7acc0a048eb
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2022-01-06 22:24:11 -0800

    Add userActions, userConstants, userReducers

---
+++ client/src/actions/userActions.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants";
+
+export const login = (email: any, password: any) => async (dispatch: any) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+    
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password }, 
+      config
+    );
+
+    dispatch({ 
+      type: USER_LOGIN_SUCCESS, 
+      payload: data  
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error: any) {
+    dispatch({ 
+      type: USER_LOGIN_FAIL, 
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,  
+    });
+  }
+} 
